02faa4cdbc41f43faec29f981cececf6b5019211
Isaac Garcia <user194091@TX038.local>
2021-09-01 18:01:11 -0500

    one commit

---
+++ Version3.0/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Version3.0
+//
+//  Created by user194091 on 9/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var a: UITextField!
+    
+    @IBOutlet weak var b: UITextField!
+    
+    @IBOutlet weak var c: UITextField!
+    
+    @IBOutlet weak var resolver: UIButton!
+    
+    @IBOutlet weak var raiz1: UITextField!
+    
+    @IBOutlet weak var raiz2: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func resolverAccion(_ sender: Any) {
+        
+        
+    }
+    
+}
+
